85c11a4009fcd5350d3970bb5bce09cce3d0d969
Adam Wood <adamwoodce@gmail.com>
2022-12-03 12:57:00 +0000

    Advent of code: Day 3.

---
+++ 3.py
@@ -0,0 +1,34 @@
+def chrToInt(char):
+    if char.isupper():
+        return ord(char) - 38
+    else:
+        return ord(char) - 96
+
+def main():
+    with open("input_3.txt", "r") as infile:
+        lines = infile.readlines()
+
+        total = 0
+        # Part 1
+        for line in lines:
+            items = int(len(line.strip())/2)
+            first = line[:items]
+            second = line[items:]
+            total += chrToInt(set.intersection(*map(set, [first, second])).pop())
+
+        print(total)
+
+        # Part 2
+        group = []
+        total = 0
+        for line in lines:
+            group.append(line.strip())
+            if len(group) == 3:
+                total += chrToInt(set.intersection(*map(set,group)).pop())
+                group = []
+
+        print(total)
+
+
+if __name__ == "__main__":
+    main()
